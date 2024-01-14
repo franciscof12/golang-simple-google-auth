2ee04bd08d0de87241684ccd714541813af3ac86
Francisco <franciscoferrefc@gmail.com>
2024-01-15 00:08:15 +0100

    🚀

---
+++ vite-login/src/App.tsx
@@ -0,0 +1,33 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:8080/auth/google'
+  }
+ 
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleLogin}>
+          Login with google
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
